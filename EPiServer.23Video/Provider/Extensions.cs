5f945499f6d3c7e7ceb13527f9e0cc1a547f0d4e
permagne <permagne@bvnetwork.com>
2014-06-18 13:23:57 +0200

    Fixed nuget package dependencies.
    Added extensions class

---
+++ EPiServer.23Video/Provider/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EPiCode.TwentyThreeVideo.Models;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+
+namespace EPiCode.TwentyThreeVideo.Provider
+{
+    public static class Extensions
+    {
+        public static IEnumerable<object> Videos(this IContent content)
+        {
+            var contentSoftLinkIndexer = ServiceLocator.Current.GetInstance<ContentSoftLinkIndexer>();
+            var links = contentSoftLinkIndexer.GetLinks(content);
+            var repository = ServiceLocator.Current.GetInstance<IContentRepository>();
+            foreach (var softLink in links)
+            {
+                Video videoContent = null;
+                if (
+                    repository
+                        .TryGet<Video>(softLink.ReferencedContentLink, out videoContent))
+                {
+                    yield return new
+                    {
+                        VideoId = ((Video)videoContent).Id,
+                        Title = videoContent.Name
+                    };
+                }
+            }
+        }
+    }
+}
