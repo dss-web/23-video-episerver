f4ea67ae8850adfe68fca0491f02053ee6361aef
Oeyvind Wabakken Hognestad <oyvind@bvnetwork.com>
2014-06-24 12:06:25 +0200

    Added controller for video to get thumbs in file explorer working.

---
+++ EPiServer.23Video/Controllers/TwentyThreeVideoController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web;
+using EPiCode.TwentyThreeVideo.Models;
+
+namespace EPiCode.TwentyThreeVideo.Controllers
+{
+    [TemplateDescriptor(TagString = EPiServer.Framework.Web.RenderingTags.Preview )]
+    public class TwentyThreeVideoController : Controller, IRenderTemplate<Video>
+    {
+        public ActionResult Index(Video currentContent)
+        {
+            return PartialView(currentContent);
+        }
+    }
+}
