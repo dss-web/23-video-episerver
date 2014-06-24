c79f1af2db90157189909146489fe14a36b884a1
Oeyvind Wabakken Hognestad <oyvind@bvnetwork.com>
2014-06-24 08:56:53 +0200

    New configurations due to new user.
    Removed oEmbed logic from controllers.

--- EPiServer.23Video/Controllers/TwentyThreeVideoPartialContentController.cs
+++ EPiServer.23Video/Controllers/TwentyThreeVideoPartialContentController.cs
@@ -9,11 +9,6 @@ namespace EPiCode.TwentyThreeVideo.Controllers
     {
         public override ActionResult Index(Video currentContent)
         {
-            if (Client.Settings.OEmbedIsEnabled)
-            {
-                currentContent.VideoUrl = currentContent.oEmbedHtml;
-            }
-
             return PartialView(currentContent);
         }
     }
