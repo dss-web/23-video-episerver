9928994465b11d67455c5bdebd9246d68a58e008
Oeyvind Wabakken Hognestad <oyvind@bvnetwork.com>
2014-06-30 09:58:32 +0200

    UIHint

---
+++ EPiServer.23Video/UI/TwentyThreeUIHint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EPiCode.TwentyThreeVideo.Models;
+using EPiServer.Core;
+using EPiServer.Shell.ObjectEditing.EditorDescriptors;
+
+namespace EPiCode.TwentyThreeVideo.UI
+{
+    [EditorDescriptorRegistration(TargetType = typeof(ContentReference), UIHint = "twentythreevideo")]
+    public class BlockReferenceEditorDescriptor : ContentReferenceEditorDescriptor<Video>
+    {
+        public override IEnumerable<ContentReference> Roots
+        {
+            get
+            {
+                return new ContentReference[] { new ContentReference(164) };
+            }
+        }
+    }
+
+}
